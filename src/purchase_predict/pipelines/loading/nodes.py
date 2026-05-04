bf790883ba3ad79642fafafe4a5749b41141d5a6
eyal29 <eyalachheb2@gmail.com>
2026-05-04 15:44:06 +0200

    trigger build

--- src/purchase_predict/pipelines/loading/nodes.py
+++ src/purchase_predict/pipelines/loading/nodes.py
@@ -32,7 +32,6 @@
 #     return df
 
 
-import glob
 import os
 import tempfile
 
@@ -44,21 +43,29 @@ def load_csv_from_bucket(project: str, bucket_path: str) -> pd.DataFrame:
     """
     Loads multiple CSV files from bucket (as generated from Spark).
     """
-    storage_client = storage.Client()
+    storage_client = storage.Client(project=project)
     bucket_name = bucket_path.split("/")[0]
     folder = "/".join(bucket_path.split("/")[1:]) + "/part-"
 
     temp_dir = tempfile.gettempdir()
+    downloaded_files = []
 
     for blob in storage_client.list_blobs(bucket_name, prefix=folder):
         filename = blob.name.split("/")[-1]
         if filename.endswith("csv"):
-            blob.download_to_filename(os.path.join(temp_dir, filename))
+            local_path = os.path.join(temp_dir, filename)
+            blob.download_to_filename(local_path)
+            downloaded_files.append(local_path)
 
-    all_files = glob.glob(os.path.join(temp_dir, "*.csv"))
+    if not downloaded_files:
+        raise ValueError(
+            f"No CSV files found in gs://{bucket_name}/{folder}. "
+            "Check gcs_primary_folder and ensure Spark output files exist (part-*.csv)."
+        )
+
     li = []
 
-    for filename in all_files:
+    for filename in downloaded_files:
         df = pd.read_csv(filename, index_col=None, sep=",")
         li.append(df)
 
